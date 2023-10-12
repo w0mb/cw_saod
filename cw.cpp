01643b3e96066b09e8e0cac3480bae1cf9882759
w0mb <danil-kovalev-04@mail.ru>
2023-10-12 22:45:33 +0700

    создано дерево и возможность поиска по этому дереву

--- cw.cpp
+++ cw.cpp
@@ -18,29 +18,82 @@ struct Record
     char CheckInDate[10];
 };
 
+struct TreeNode
+{
+    Record *record;
+    TreeNode *left;
+    TreeNode *right;
+};
+
 struct QueueElement
 {
-    Record* record;
-    QueueElement* next;
+    Record *record;
+    QueueElement *next;
 };
 
 struct RecordQueue
 {
-    QueueElement* front;
-    QueueElement* rear;
+    QueueElement *front;
+    QueueElement *rear;
     int count;
 };
 
-void InitializeRecordQueue(RecordQueue& queue)
+TreeNode *InsertIntoSearchTree(TreeNode *root, Record *record)
+{
+    if (root == nullptr)
+    {
+        TreeNode *newNode = new TreeNode;
+        newNode->record = record;
+        newNode->left = nullptr;
+        newNode->right = nullptr;
+        return newNode;
+    }
+
+    if (strncmp(record->Street, root->record->Street, 18) < 0)
+    {
+        root->left = InsertIntoSearchTree(root->left, record);
+    }
+    else
+    {
+        root->right = InsertIntoSearchTree(root->right, record);
+    }
+
+    return root;
+}
+
+Record *SearchInSearchTree(TreeNode *root, const char *key)
+{
+    if (root == nullptr)
+    {
+        return nullptr;
+    }
+
+    int cmp = strncmp(key, root->record->Street, 18);
+
+    if (cmp == 0)
+    {
+        return root->record;
+    }
+    else if (cmp < 0)
+    {
+        return SearchInSearchTree(root->left, key);
+    }
+    else
+    {
+        return SearchInSearchTree(root->right, key);
+    }
+}
+
+void InitializeRecordQueue(RecordQueue &queue)
 {
     queue.front = nullptr;
     queue.rear = nullptr;
     queue.count = 0;
 }
 
-void Enqueue(RecordQueue& queue, Record* record)
+void Enqueue(RecordQueue &queue, Record *record)
 {
-    QueueElement* newElement = new QueueElement;
+    QueueElement *newElement = new QueueElement;
     newElement->record = record;
     newElement->next = nullptr;
 
@@ -58,18 +111,27 @@ void Enqueue(RecordQueue& queue, Record* record)
     queue.count++;
 }
 
-void DisplayQueue(const RecordQueue& queue)
+void DisplayQueue(const RecordQueue &queue, int currentPage)
 {
     const int RecordsPerPage = 20;
-    QueueElement* current = queue.front;
+    QueueElement *current = queue.front;
     int recordCount = 0;
 
-    while (current != nullptr)
+    while (true)
     {
+        if (current == nullptr)
+        {
+            system("cls");
+            cout << "Queue is empty." << endl;
+            cout << "Press any key to return to the menu...";
+            _getch();
+            break;
+        }
+
         if (recordCount % RecordsPerPage == 0)
         {
             system("cls");
-            cout << "Queue Contents:" << endl;
+            cout << "Queue Contents (Page " << currentPage + 1 << "):" << endl;
             cout << "Num\tFull Name\t\t\tStreet\t\tHouse Number\tApartment Number\tCheck-In Date" << endl;
         }
 
@@ -82,19 +144,16 @@ void DisplayQueue(const RecordQueue& queue)
 
         if (recordCount % RecordsPerPage == 0)
         {
-            cout << "Press 'Q' to Quit or any other key to continue...";
-
+            cout << "Press 'Q' to return to the menu, Left Arrow for Previous Page, or any other key to continue...";
             char Input = _getch();
+
             if (Input == 'Q' || Input == 'q')
                 break;
+            else if (Input == 75 && currentPage > 0)
+                return; // Previous page
         }
     }
-
-    cout << "Press any key to continue...";
-    _getch();
 }
-
-
 
 struct RecordArray
 {
@@ -102,12 +161,12 @@ struct RecordArray
     int count;
 };
 
-void InitializeRecordArray(RecordArray& arr)
+void InitializeRecordArray(RecordArray &arr)
 {
     arr.count = 0;
 }
 
-void AddRecord(RecordArray& arr, const Record& record)
+void AddRecord(RecordArray &arr, const Record &record)
 {
     if (arr.count < MaxRecords)
     {
@@ -115,73 +174,72 @@ void AddRecord(RecordArray& arr, const Record& record)
     }
 }
 
-void DisplayRecords(const RecordArray& arr)
+void DisplaySearchTree(TreeNode *root, int &currentPage, int totalPages);
+
+void DisplayRecords(const RecordArray &arr, int currentPage, int totalPages)
 {
     const int RecordsPerPage = 20;
-    int TotalPages = (arr.count + RecordsPerPage - 1) / RecordsPerPage;
-    int CurrentPage = 0;
+    int startIdx = currentPage * RecordsPerPage;
+    int endIdx = min(startIdx + RecordsPerPage, arr.count);
 
-    while (true)
+    system("cls");
+    cout << "Page " << currentPage + 1 << "/" << totalPages << ":" << endl;
+    cout << "Num\tFull Name\t\t\tStreet\t\tHouse Number\tApartment Number\tCheck-In Date" << endl;
+
+    for (int j = startIdx; j < endIdx; ++j)
     {
-        system("cls");
-
-        cout << "Page " << CurrentPage + 1 << "/" << TotalPages << ":" << endl;
-        cout << "Num\tFull Name\t\t\tStreet\t\tHouse Number\tApartment Number\tCheck-In Date" << endl;
-
-        int startIdx = CurrentPage * RecordsPerPage;
-        int endIdx = min(startIdx + RecordsPerPage, arr.count);
-
-        for (int j = startIdx; j < endIdx; ++j)
-        {
-            const Record& record = arr.records[j];
-            cout << j + 1 << "\t" << record.FullName << "\t" << record.Street << "\t" << record.HouseNumber
-                 << "\t\t" << record.ApartmentNumber << "\t\t" << record.CheckInDate << endl;
-        }
-
-        cout << "Use Left Arrow for Previous Page, Right Arrow for Next Page, 'Q' to Quit, or enter page number and press Enter: ";
-
-        char Input = _getch();
-
-        if (Input == 'Q' || Input == 'q')
-        {
-            break;
-        }
-        else if (Input == 77)
-        {
-            if (CurrentPage < TotalPages - 1)
-                CurrentPage++;
-        }
-        else if (Input == 75)
-        {
-            if (CurrentPage > 0)
-                CurrentPage--;
-        }
-        else if (isdigit(Input))
-        {
-            int Page = Input - '0';
-            cout << Page;
-            Sleep(100);
-
-            char NextChar;
-            do
-            {
-                NextChar = _getch();
-                if (isdigit(NextChar))
-                {
-                    Page = Page * 10 + (NextChar - '0');
-                    cout << NextChar;
-                }
-            } while (isdigit(NextChar));
-
-            if (Page >= 1 && Page <= TotalPages)
-                CurrentPage = Page - 1;
-        }
+        const Record &record = arr.records[j];
+        cout << j + 1 << "\t" << record.FullName << "\t" << record.Street << "\t" << record.HouseNumber
+             << "\t\t" << record.ApartmentNumber << "\t\t" << record.CheckInDate << endl;
     }
+
+    cout << "Use Left Arrow for Previous Page, Right Arrow for Next Page, 'Q' to Quit, or enter page number and press Enter: ";
 }
 
-void SearchAndDisplay(RecordArray& arr, RecordQueue& searchQueue)
+int Navigate(int currentPage, int totalPages)
 {
-    char Key[4]; // To store the search key (first 3 letters)
+    char Input = _getch();
+
+    if (Input == 'Q' || Input == 'q')
+    {
+        return -1; // Exit
+    }
+    else if (Input == 77)
+    {
+        if (currentPage < totalPages - 1)
+            return currentPage + 1; // Next page
+    }
+    else if (Input == 75)
+    {
+        if (currentPage > 0)
+            return currentPage - 1; // Previous page
+    }
+    else if (isdigit(Input))
+    {
+        int Page = Input - '0';
+
+        char NextChar;
+        do
+        {
+            NextChar = _getch();
+            if (isdigit(NextChar))
+            {
+                Page = Page * 10 + (NextChar - '0');
+            }
+        } while (isdigit(NextChar));
+
+        if (Page >= 1 && Page <= totalPages)
+            return Page - 1;
+    }
+
+    return currentPage;
+}
+
+
+
+void SearchAndDisplay(RecordArray &arr, RecordQueue &searchQueue, TreeNode *&searchTreeRoot)
+{
+    char Key[4];
     cout << "Enter the search key (first 3 letters of Full Name): ";
     cin.getline(Key, sizeof(Key));
 
@@ -193,28 +251,37 @@ void SearchAndDisplay(RecordArray& arr, RecordQueue& searchQueue)
         if (strncmp(arr.records[i].FullName, Key, 3) == 0)
         {
             AddRecord(MatchingRecords, arr.records[i]);
-            // Добавить запись в очередь searchQueue
             Enqueue(searchQueue, &arr.records[i]);
+            searchTreeRoot = InsertIntoSearchTree(searchTreeRoot, &arr.records[i]);
         }
     }
 
-    if (MatchingRecords.count == 0)
+        if (MatchingRecords.count == 0)
     {
         cout << "No matching records found." << endl;
     }
     else
     {
         cout << "Matching records:" << endl;
-        DisplayRecords(MatchingRecords);
+
+        int totalPages = (MatchingRecords.count + 20 - 1) / 20;
+        int currentPage = 0;
+
+        while (true)
+        {
+            DisplayRecords(MatchingRecords, currentPage, totalPages);
+            currentPage = Navigate(currentPage, totalPages);
+
+            if (currentPage == -1)
+                break;
+        }
     }
 
-    // Wait for a key press before returning to the menu
-    cout << "Press any key to continue...";
+    cout << "Press any key to continue";
     _getch();
 }
 
-
-void HoareSort(RecordArray& arr, int first, int last)
+void HoareSort(RecordArray &arr, int first, int last)
 {
     int i = first, j = last;
     Record tmp, x = arr.records[(first + last) / 2];
@@ -247,6 +314,73 @@ void HoareSort(RecordArray& arr, int first, int last)
         HoareSort(arr, first, j);
 }
 
+void DisplaySearchTree(TreeNode *root, RecordQueue &searchQueue, int currentPage, int totalPages)
+{
+    if (root == nullptr)
+    {
+        return;
+    }
+
+    const int RecordsPerPage = 20;
+    QueueElement *current = searchQueue.front;
+    int recordCount = 0;
+
+    while (true)
+    {
+        if (current == nullptr)
+        {
+            system("cls");
+            cout << "No matching records found in the Search Tree." << endl;
+            cout << "Press any key to return to the menu...";
+            _getch();
+            break;
+        }
+
+        if (recordCount % RecordsPerPage == 0)
+        {
+            system("cls");
+            cout << "Search Tree Contents (Page " << currentPage + 1 << "):" << endl;
+            cout << "Num\tFull Name\t\t\tStreet\t\tHouse Number\tApartment Number\tCheck-In Date" << endl;
+        }
+
+        cout << recordCount + 1 << "\t" << current->record->FullName << "\t" << current->record->Street
+             << "\t" << current->record->HouseNumber << "\t\t" << current->record->ApartmentNumber
+             << "\t\t" << current->record->CheckInDate << endl;
+
+        current = current->next;
+        recordCount++;
+
+        if (recordCount % RecordsPerPage == 0)
+        {
+            cout << "Press 'Q' to return to the menu, Left Arrow for Previous Page, or any other key to continue...";
+            char Input = _getch();
+
+            if (Input == 'Q' || Input == 'q')
+                break;
+        }
+    }
+}
+
+// Функция для отображения записей, соответствующих поисковому запросу
+void DisplayMatchingRecords(const RecordArray &arr, int currentPage, int totalPages)
+{
+    const int RecordsPerPage = 20;
+    int startIdx = currentPage * RecordsPerPage;
+    int endIdx = min(startIdx + RecordsPerPage, arr.count);
+
+    system("cls");
+    cout << "Matching Records (Page " << currentPage + 1 << "/" << totalPages << "):" << endl;
+    cout << "Num\tFull Name\t\t\tStreet\t\tHouse Number\tApartment Number\tCheck-In Date" << endl;
+
+    for (int j = startIdx; j < endIdx; ++j)
+    {
+        const Record &record = arr.records[j];
+        cout << j + 1 << "\t" << record.FullName << "\t" << record.Street << "\t" << record.HouseNumber
+             << "\t\t" << record.ApartmentNumber << "\t\t" << record.CheckInDate << endl;
+    }
+
+    cout << "Use Left Arrow for Previous Page, Right Arrow for Next Page, 'Q' to Quit, or enter page number and press Enter: ";
+}
 int main()
 {
     FILE *fp;
@@ -256,7 +390,6 @@ int main()
         cout << "Error opening file." << endl;
         return 1;
     }
-
     RecordArray Database;
     InitializeRecordArray(Database);
 
@@ -269,14 +402,16 @@ int main()
     Database.count = NumberOfRecords;
     cout << "Total records: " << NumberOfRecords << endl;
 
-    // Make a copy of the sorted database
     RecordArray SortedDatabase = Database;
-
-    // Sort the copy using HoareSort
     HoareSort(SortedDatabase, 0, SortedDatabase.count - 1);
 
-    RecordQueue searchQueue; // Очередь для записей с одинаковым ключом поиска
+    RecordQueue searchQueue;
     InitializeRecordQueue(searchQueue);
+
+    TreeNode *searchTreeRoot = nullptr;
+
+    RecordArray MatchingRecords; // Объявим MatchingRecords здесь
+    InitializeRecordArray(MatchingRecords);
 
     while (true)
     {
@@ -287,37 +422,114 @@ int main()
         cout << "2. View Sorted Database" << endl;
         cout << "3. Search by Key (Full Name)" << endl;
         cout << "4. Display Search Queue" << endl;
-        cout << "5. Exit" << endl;
+        cout << "5. Display Search Tree" << endl;
+        cout << "6. Search in Search Tree" << endl;
+        cout << "7. Exit" << endl;
 
         char Input = _getch();
 
         if (Input == '1')
         {
-            DisplayRecords(Database);
+            int currentPage = 0;
+            int totalPages = (Database.count + 20 - 1) / 20;
+
+            while (true)
+            {
+                DisplayRecords(Database, currentPage, totalPages);
+                currentPage = Navigate(currentPage, totalPages);
+
+                if (currentPage == -1)
+                    break;
+            }
         }
         else if (Input == '2')
         {
-            DisplayRecords(SortedDatabase);
+            int currentPage = 0;
+            int totalPages = (SortedDatabase.count + 20 - 1) / 20;
+
+            while (true)
+            {
+                DisplayRecords(SortedDatabase, currentPage, totalPages);
+                currentPage = Navigate(currentPage, totalPages);
+
+                if (currentPage == -1)
+                    break;
+            }
         }
         else if (Input == '3')
         {
-            SearchAndDisplay(SortedDatabase, searchQueue); // Search in the sorted copy
+            SearchAndDisplay(SortedDatabase, searchQueue, searchTreeRoot);
         }
         else if (Input == '4')
         {
-            DisplayQueue(searchQueue); // Отобразить содержимое очереди
+            int currentPage = 0;
+            while (true)
+            {
+                DisplayQueue(searchQueue, currentPage);
+                char SubInput = _getch();
+
+                if (SubInput == 'Q' || SubInput == 'q')
+                    break;
+                else if (SubInput == 75 && currentPage > 0)
+                    currentPage--; // Previous page
+            }
         }
-        else if (Input == '5')
+ if (Input == '5')
+    {
+        int currentPage = 0;
+        int totalPages = (/* Calculate the total number of nodes in the search tree */ + 20 - 1) / 20;
+
+        while (true)
+        {
+            DisplaySearchTree(searchTreeRoot, searchQueue, currentPage, totalPages);
+            char SubInput = _getch();
+
+            if (SubInput == 'Q' || SubInput == 'q')
+                break;
+        }
+    }
+    else if (Input == '6')
+    {
+        // Поиск в дереве
+        char searchKey[18];
+        cout << "Enter the search key (Street) to search in the Search Tree: ";
+        cin.getline(searchKey, sizeof(searchKey));
+
+        Record *foundRecord = SearchInSearchTree(searchTreeRoot, searchKey);
+
+        if (foundRecord)
+        {
+            int currentPage = 0;
+            int totalPages = (MatchingRecords.count + 20 - 1) / 20;
+
+            while (true)
+            {
+                DisplayMatchingRecords(MatchingRecords, currentPage, totalPages);
+                currentPage = Navigate(currentPage, totalPages);
+
+                if (currentPage == -1)
+                    break;
+            }
+        }
+        else
+        {
+            cout << "No matching records found in the Search Tree." << endl;
+        }
+
+        cout << "Press any key to continue";
+        _getch();
+    }
+        else if (Input == '7')
         {
             break;
         }
     }
 
-    // Освобождаем память, выделенную для элементов очереди
-    QueueElement* current = searchQueue.front;
+    // Free memory allocated for queue elements
+    QueueElement *current = searchQueue.front;
     while (current != nullptr)
     {
-        QueueElement* temp = current;
+        QueueElement *temp = current;
         current = current->next;
         delete temp;
     }
@@ -325,5 +537,4 @@ int main()
     fclose(fp);
     return 0;
 }
-
 
